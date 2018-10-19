6b3f1759e5afff66f9332ae99b43e055ce4229d0
psiang <psiang@163.com>
2018-10-19 15:00:41 +0800

    first commit

---
+++ lab1.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <cstdlib>
+
+int main() {
+    int  i = 0;
+    printf("C:\\Program Files (x86)\\Dev-Cpp\\MinGW64\\bin\\gdb.exe\n");
+    system("pause");
+    return 0;
+}
